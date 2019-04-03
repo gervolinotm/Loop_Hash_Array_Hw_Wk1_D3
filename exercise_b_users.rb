00889cad8a670926756638a40187c61082534ca1
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-03 16:18:38 +0100

    Exercise b - Erik's lowest valued lottery number

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -63,6 +63,7 @@ p users["Erik"][:lottery_numbers]
 # 4. Get the type of Avril's pet Monty
 p users["Avril"][:pets][0][:species]
 # 5. Get the smallest of Erik's lottery numbers
+p users["Erik"][:lottery_numbers].sort[0]
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
