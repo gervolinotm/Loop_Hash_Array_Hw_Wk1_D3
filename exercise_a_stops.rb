ef57b1f4bcb9ad99e0ff94a8dfdee0066c586bee
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-03 15:47:42 +0100

    Removed Livingston

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -1,14 +1,20 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
-p stops << "Edinburgh Waverley"
+stops << "Edinburgh Waverley"
+p stops
 # 2. Add `"Glasgow Queen St"` to the start of the array
-p stops.unshift("Glasgow Queen St")
+stops.unshift("Glasgow Queen St")
+p stops
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-p stops.insert(4, "Polmont")
+stops.insert(4, "Polmont")
+p stops
 # 4. Work out the index position of `"Linlithgow"`
-p stops.index("Linlithgow")
+stops.index("Linlithgow")
+p stops
 # 5. Remove `"Livingston"` from the array using its name
+stops.delete("Livingston")
+p stops
 # 6. Delete `"Cumbernauld"` from the array by index
 # 7. How many stops there are in the array?
 # 8. How many ways can we return `"Falkirk High"` from the array?
